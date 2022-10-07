a50d233e1343142693eefa2bcc436a7fb6941be0
hyun_99 <hshiner99@gmail.com>
2022-10-07 18:10:44 +0900

    uouiu

--- src/components/ChallengeModal.jsx
+++ src/components/ChallengeModal.jsx
@@ -5,8 +5,15 @@ const ChallengeModal = ({ isOpen, setModalState, data }) => {
   return (
     <ReactModal
       isOpen={isOpen}
+      marginLeft={"300px"}
       onRequestClose={() => setModalState({ data: {}, isOpen: false })}
     >
+      <div style={{
+        marginLeft: "300px"
+      }}>
+
+      </div>
+      <p>하이?</p>
       <p>title : {data.title}</p>
       <p>score : {data.score}</p>
     </ReactModal>
