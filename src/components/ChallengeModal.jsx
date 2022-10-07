bf9c664609fd2fe4e7bbe0da1bb9f4783701bd44
H4uN <dohyun0822@gmail.com>
2022-10-07 15:38:17 +0900

    chall

--- src/components/ChallengeModal.jsx
+++ src/components/ChallengeModal.jsx
@@ -6,6 +6,36 @@ const ChallengeModal = ({ isOpen, setModalState, data }) => {
     <ReactModal
       isOpen={isOpen}
       onRequestClose={() => setModalState({ data: {}, isOpen: false })}
+      style={{
+        overlay: {
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundcolor: "rgba(0, 0, 0, 0.4)",
+          display: "flex",
+          justifycontent: "center",
+          alignitems: "center",
+        },
+        content: {
+          position: "absolute",
+          top: "25%",
+          left: "400px",
+          right: "15%",
+          bottom: "25%",
+          border: "1px solid #ccc",
+          background: "#fff",
+          overflow: "auto",
+          WebkitOverflowScrolling: "touch",
+          borderRadius: "4px",
+          outline: "none",
+          padding: "20px",
+          textalign: "center",
+          verticalalign: "middle",
+          textAlign: "center",
+        },
+      }}
     >
       <p>title : {data.title}</p>
       <p>score : {data.score}</p>
