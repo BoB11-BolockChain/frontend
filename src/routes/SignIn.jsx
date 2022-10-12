368d90e0ecb1ce9901cb6c8de19ec8cf3640ad2a
hyun_99 <hshiner99@gmail.com>
2022-10-12 14:44:09 +0900

    로그인 세션 수정, 게시판 추가

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -24,7 +24,7 @@ const SignIn = () => {
     });
     const js = await res.json();
     console.log(js);
-    window.sessionStorage.setItem("sessionId", "fdsa");
+    window.sessionStorage.setItem("sessionId", js.sessionId);
     navigate("/admin");
   };
 
