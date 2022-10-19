4ae6a0f9201ebc5c1038e2657656ec9ed31f9679
H4uN <dohyun0822@gmail.com>
2022-10-19 17:11:21 +0900

    asdf

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "src/components/Layout/module.scss";
+
+const Header = () => {
+  return (
+    <>
+      <h1> asdf1</h1>
+    </>
+  );
+};
+
+export default Header;
