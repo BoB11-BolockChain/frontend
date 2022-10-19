814a0f5490c52554dee39268455b83fcd7a59540
Park, Chang Sun <changsun19991212@gmail.com>
2022-10-19 22:04:09 +0900

    design: dashboard

--- src/routes/Challenges.jsx
+++ src/routes/Challenges.jsx
@@ -3,55 +3,86 @@ import ChallengeModal from "src/components/ChallengeModal";
 import Layout from "src/components/Layout/Layout";
 import ReactiveButton from "reactive-button";
 import Loading from "src/components/Loading";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
+import useGetFetch from "src/hooks/useGetFetch";
 
 const Challenges = () => {
-  const [dataLoaded, setDataLoaded] = useState(false);
-  const [data, setData] = useState([]);
   const [modalState, setModalState] = useState({ data: {}, isOpen: false });
+  const [isFetched, data] = useGetFetch("http://www.pdxf.tk:8000/info");
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://www.pdxf.tk:8000/info", {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      });
-      if (res.ok) {
-        const js = await res.json();
-        setData(js.data);
-        setDataLoaded(true);
-      }
-    };
-
-    fetchData();
+    console.log(data);
   }, []);
 
-  return (
-    <Layout>
-      <header>
+  return !isFetched ? (
+    <Loading />
+  ) : (
+    <>
+      <Layout>
         <h1>challenges</h1>
-      </header>
-      {dataLoaded ? (
-        data.map((d) => (
-          <div key={d.title}>
-            <p>
-              <ReactiveButton
-                onClick={() => setModalState({ data: d, isOpen: true })}
-                idleText={<>{d.title}<br />{d.score}</>} /></p>
-          </div>
-        ))
-      ) : (
-        <Loading />
-      )}
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <div>item</div>
+          </Grid>
+          <Grid item xs={4}>
+            <div>item</div>
+          </Grid>
+          {data.data.map((d) => (
+            <Grid item xs={4}>
+              <Card variable="outlined">
+                <CardContent>
+                  <Typography align="center">center text</Typography>
+                  <p>{d.title}</p>
+                  <Button
+                    variant="contained"
+                    onClick={() => setModalState({ data: d, isOpen: true })}
+                  >
+                    {d.score}
+                  </Button>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Layout>
       <ChallengeModal
         isOpen={modalState.isOpen}
         setModalState={setModalState}
         data={modalState.data}
       />
-    </Layout>
+    </>
   );
+  // return (
+  //   <Layout>
+  //     <header>
+  //       <h1>challenges</h1>
+  //     </header>
+  //     {dataLoaded ? (
+  //       data.map((d) => (
+  //         <div key={d.title}>
+  //           <p>
+  //             <ReactiveButton
+  //               onClick={() => setModalState({ data: d, isOpen: true })}
+  //               idleText={<>{d.title}<br />{d.score}</>} /></p>
+  //         </div>
+  //       ))
+  //     ) : (
+  //       <Loading />
+  //     )}
+  //     <ChallengeModal
+  //       isOpen={modalState.isOpen}
+  //       setModalState={setModalState}
+  //       data={modalState.data}
+  //     />
+  //   </Layout>
+  // );
 };
 
 export default Challenges;
--- src/routes/private/DashboardById.jsx
+++ src/routes/private/DashboardById.jsx
@@ -1,3 +1,15 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Modal,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "src/components/Layout/Layout";
@@ -7,33 +19,107 @@ import useWebSocket from "src/hooks/useWebSocket";
 
 const DashboardById = () => {
   const { id } = useParams();
-  const msg = useWebSocket(`ws://www.pdxf.tk:8000/dashboard?id=${id}`);
-  const [steps, setSteps] = useState(null);
+  // const msg = useWebSocket(`ws://www.pdxf.tk:8000/dashboard?id=${id}`);
+  // const [steps, setSteps] = useState(null);
 
-  useEffect(() => {
-    if (msg) {
-      console.log(id);
-      const json = JSON.parse(msg);
-      const paw = json["host_group"][0]["paw"];
-      setSteps(json["steps"][paw]["steps"]);
-    }
-  }, [msg]);
+  // useEffect(() => {
+  //   if (msg) {
+  //     console.log(id);
+  //     const json = JSON.parse(msg);
+  //     const paw = json["host_group"][0]["paw"];
+  //     setSteps(json["steps"][paw]["steps"]);
+  //   }
+  // }, [msg]);
 
-  return !msg || !steps ? (
-    <Loading />
-  ) : (
+  const [modalState, setModalState] = useState({ data: {}, isOpen: false });
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "white",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const dummy = [
+    {
+      id: "dsafdsafsa",
+      seq: "whoami",
+      time: "2022-22-22",
+      status: 0,
+    },
+    {
+      id: "dsafdsafsafdsafa",
+      seq: "superpowershell",
+      time: "2022-22-22",
+      status: 1,
+    },
+    {
+      id: "dsafdsafsadsfafsaf",
+      seq: "go home home",
+      time: "2022-22-22",
+      status: 1,
+    },
+  ];
+
+  return (
     <Layout>
-      <p>progress dashboard</p>
-      <ul>
-        {steps.map((step) => (
-          <li key={step.ability_id}>
-            <p>userid: {step.name}</p>
-            <p>status: {step.status}</p>
-            <p>time: {step.run}</p>
-            <p>{step.description}</p>
-          </li>
-        ))}
-      </ul>
+      <Typography variant="h3">Dashboard of {id}</Typography>
+      <Box
+        sx={{
+          m: 4,
+          border: "2px solid grey",
+          "border-radius": "15px",
+          backgroundColor: "white",
+        }}
+      >
+        <Table>
+          <TableHead>
+            <TableCell></TableCell>
+            <TableCell>attack sequence</TableCell>
+            <TableCell>timestamp</TableCell>
+            <TableCell>see status</TableCell>
+          </TableHead>
+          <TableBody>
+            {dummy.map((d, i) => (
+              <TableRow key={d.id}>
+                <TableCell>
+                  <Avatar
+                    variant="rounded"
+                    sx={{ bgcolor: d.status === 0 ? "green" : "red" }}
+                  >
+                    {i}
+                  </Avatar>
+                </TableCell>
+                <TableCell>{d.seq}</TableCell>
+                <TableCell>{d.time}</TableCell>
+                <TableCell>
+                  <Button
+                    onClick={() => setModalState({ data: d, isOpen: true })}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    see status
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+      <Modal
+        open={modalState.isOpen}
+        onClose={() => setModalState({ data: {}, isOpen: false })}
+      >
+        <Box sx={style}>
+          <Typography align="center">{modalState.data.seq}</Typography>
+          <Typography align="center">{modalState.data.status}</Typography>
+        </Box>
+      </Modal>
     </Layout>
   );
 };
