0ff6473380be4b953a8abc91557a144ca5354120
Park, Chang Sun <changsun19991212@gmail.com>
2022-12-17 02:31:57 +0900

    score image

--- src/components/ScoreTBody.jsx
+++ src/components/ScoreTBody.jsx
@@ -1,4 +1,11 @@
 const ScoreTBody = ({ data, setModalState, img }) => {
+  const loadImg = (user) => {
+    try {
+      return require(`src/assets/${user}.png`);
+    } catch (error) {
+      return img;
+    }
+  };
   return (
     <tbody>
       {data.map((data, i) => (
@@ -6,9 +13,9 @@ const ScoreTBody = ({ data, setModalState, img }) => {
           key={i}
           onClick={() => setModalState({ data: data, isOpen: true, img: img })}
         >
-          <td>{i+1}</td>
+          <td>{i + 1}</td>
           <td>
-            <img src={img} height="30px" />
+            <img src={loadImg(data.id)} height="30px" />
           </td>
           <td id="id">{data.id}</td>
           <td>{data.org}</td>
